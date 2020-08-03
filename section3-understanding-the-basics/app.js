74f0495673bd50070dbe7cc3aaf662d91b3f420b
blksim <blakesim3@gmail.com>
2020-08-03 18:03:43 +0900

    section 3

---
+++ section3-understanding-the-basics/app.js
@@ -0,0 +1,10 @@
+const http = require('http');// import global module
+// the file content here is actually cached by node and we can't edit it externally,
+// and we can't edit it externally.
+// so if we somehow would define routes as an object and we tried to add a new property on the fly here, this would not manipulate the original file.
+// so this is basically locked, not accessible from outside.
+const routes = require('./routes');
+const server = http.createServer(routes.hnandler);
+
+console.log(routes.someText);
+server.listen(3000); // starts a process
