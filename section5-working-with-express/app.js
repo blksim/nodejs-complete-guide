5c229f25d691b715c6503dc0c27ce268905afc14
blksim <blakesim3@gmail.com>
2020-08-05 11:43:07 +0900

    section 6 w.Pug

--- section5-working-with-express/app.js
+++ section5-working-with-express/app.js
@@ -3,19 +3,27 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-const adminRoutes = require('./routes/admin');
+
+// assigns setting name to value. certain names can be used to configure the behavior of the server.
+// becuase we installed the pug templating engine, this engine actually ships with built in express support and autoregisters itself with express.
+app.engine('pug', require('pug').__express); 
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 // It will do while request body parsing we had to do manually in the previous section.
 // This will not parse all kinds of possible bodies. This will parse bodies like sent trhough the form.
 app.use(bodyParser.urlencoded({extended: false})); 
-app.use('/admin', adminRoutes);
-
-app.use(shopRoutes);
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/admin', adminData.routes);
+app.use(shopRoutes);
+
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+//  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+  res.status(404).render('404', {pageTitle: '404 error'});
 });
 
 app.listen(3000);
--- section5-working-with-express/routes/shop.js
+++ section5-working-with-express/routes/shop.js
@@ -1,12 +1,13 @@
-const path = require('path'); // we can send a file where we create a path with the help of this module by calling join()
 const express = require('express');
 const router = express.Router();
-const rootDir = require('../util/path');
+const adminData = require('./admin');
 
 router.get('/', (req, res) => {
-  res.sendFile(path.join(rootDir, '../', 'views', 'shop.html')); 
+//  res.sendFile(path.join(rootDir, '../', 'views', 'shop.html')); 
   // we use path.join() because this will automatically build the path 
-  // in a way that works on both Linux and Windows systems.
+  // in a way that works  on both Linux and Windows systems.
+  const products = adminData.products;
+  res.render('shop', {prods: products, pageTitle: 'Shop', path:'/shop'});
 });
 
 module.exports = router;
