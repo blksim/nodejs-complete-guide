c99299793e95d8f52e773f1da23b46e8d97bd62c
blksim <blakesim3@gmail.com>
2020-08-05 18:18:53 +0900

    section 6 w.ejs - spent 5hrs and finally caught typosgit statusgit status

--- section5-working-with-express/app.js
+++ section5-working-with-express/app.js
@@ -1,22 +1,24 @@
 const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
-const expressHbs = require('express-handlebars');
-
+//const expressHbs = require('express-handlebars');
 const app = express();
 
 // assigns setting name to value. certain names can be used to configure the behavior of the server.
 // becuase we installed the pug templating engine, this engine actually ships with built in express support and autoregisters itself with express.
-app.engine('hbs', expressHbs({
+/* app.engine('hbs', expressHbs({
   layoutsDir: 'views/layouts/',
   defaultLayout: 'main-layout',
   extname: 'hbs'
-}));
-app.set('view engine', 'hbs'); // has to match with engine key
+})); */
+
+app.set('view engine', 'ejs');
+
+//app.set('view engine', 'hbs'); // has to match with engine key
 //app.engine('pug', require('pug').__express); 
 //app.set('view engine', 'pug');
-//app.set('views', 'views');
-app.set('views', path.join(__dirname, 'views'))
+app.set('views', 'views');
+//app.set('views', path.join(__dirname, 'views'))
 
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
